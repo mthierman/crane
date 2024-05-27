e127af7fa8186a7e542099abfa7a26784fcf6766
mthierman <mthierman@gmail.com>
2024-05-27 13:29:04 -0600

    Consolidate

--- branch.ps1
+++
@@ -1,5 +0,0 @@
-$request = Invoke-WebRequest -Uri "https://github.com/nlohmann/json/archive/refs/tags/develop.zip" -SkipHttpErrorCheck
-
-if ($request.StatusCode -eq "404") { $request = Invoke-WebRequest -Uri "https://github.com/nlohmann/json/archive/refs/heads/develop.zip" }
-
-if ($request.StatusCode -ne "404") { "Success!" }
--- crane.ps1
+++ crane.ps1
@@ -1,12 +1,48 @@
 $packages = Get-Content -Path ".\crane.json" | ConvertFrom-Json
 
+if (Test-Path "libs") { Remove-Item "libs" -Force -Recurse }
+
 foreach ($package in $packages)
 {
-    $user = $package.user
-    $repo = $package.repo
-    $tag = $package.tag
+    $split = $package -split '[@/]'
 
-    $url = "https://github.com/$user/$repo/archive/refs/tags/$tag.zip"
-    
-    Invoke-WebRequest -Uri $url | ForEach-Object { [System.IO.Compression.ZipFile]::ExtractToDirectory($_.RawContentStream, "libs/$user") }
+    $crane = [pscustomobject]@{
+        user = $split[0]
+        repo = $split[1]
+        tag  = $split[2]
+    }
+
+    $crane | Format-List
+
+    $tags = "https://github.com/$($crane.user)/$($crane.repo)/archive/refs/tags/$($crane.tag).zip"
+    $heads = "https://github.com/$($crane.user)/$($crane.repo)/archive/refs/heads/$($crane.tag).zip"
+    $request = Invoke-WebRequest -Uri $tags -SkipHttpErrorCheck
+
+    if ($request.StatusCode -eq "200")
+    {
+        foreach ($response in $request)
+        {
+            $response.StatusCode
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
+        }
+    }
+    else
+    {
+        $request = Invoke-WebRequest -Uri $heads -SkipHttpErrorCheck
+
+        if ($request.StatusCode -eq "200")
+        {
+            foreach ($response in $request)
+            {
+                $response.StatusCode
+                [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
+            }
+        }
+        else
+        {
+            gh release download -p "$($crane.tag)" -R "$($crane.user)/$($crane.repo)" --clobber
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($($crane.tag), "libs/$($crane.user)/$($crane.repo)")
+            if (Test-Path "$($crane.tag)") { Remove-Item -Path "$($crane.tag)" -Force }
+        }
+    }
 }
