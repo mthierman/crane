314df9124607dd1946cc96a58742bd9679c7cfb1
mthierman <mthierman@gmail.com>
2024-05-27 01:13:07 -0600

    pscustomobject

--- test.ps1
+++ test.ps1
@@ -1,3 +1,18 @@
 $packages = Get-Content -Path ".\test.json" | ConvertFrom-Json
 
-$packages
+foreach ($package in $packages)
+{
+    $split = $package -split '[@/]'
+
+    $crane = [pscustomobject]@{
+        user = $split[0]
+        repo = $split[1]
+        tag  = $split[2]
+    }
+
+    $obj | Format-List
+
+    $url = "https://github.com/$user/$repo/archive/refs/tags/$tag.zip"
+    
+    Invoke-WebRequest -Uri $url | ForEach-Object { [System.IO.Compression.ZipFile]::ExtractToDirectory($_.RawContentStream, "libs/$user") }
+}
