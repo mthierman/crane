9f3f4c74c009f05c3539aa0296ca8fbc0ed46c22
mthierman <mthierman@gmail.com>
2024-05-27 00:05:58 -0600

    Working

--- test.ps1
+++ test.ps1
@@ -2,13 +2,17 @@
 
 $json = Get-Content -Path ".\github.json" | ConvertFrom-Json
 
-$json.PSObject.Properties.Value | ForEach-Object -Process {
-    Write-Host $_.PSObject.Properties.Name
-    Write-Host $_.PSObject.Properties.Value
-    # $_.PSObject.Properties.Name
-    # $_.PSObject.Properties.Value
-    # $_.PSObject.Properties.Name
-    # "https://github.com/$($_.PSObject.Properties.Name)/archive/refs/tags/$($_.PSObject.Properties.Value.tag).zip"
+foreach ($repo in $json.PSObject.Properties.Value)
+{
+    # $repo
+    foreach ($repo in $repo.PSObject.Properties)
+    {
+        Write-Host $repo.Name
+        Write-Host $repo.Value.tag
+        Write-Host "https://github.com/$($repo.Name)/archive/refs/tags/$($repo.Value.tag).zip"
+
+        Invoke-WebRequest -Uri "https://github.com/$($repo.Name)/archive/refs/tags/$($repo.Value.tag).zip" | ForEach-Object { [System.IO.Compression.ZipFile]::ExtractToDirectory($_.RawContentStream, "libs/$($repo.Name)/$($repo.Value.tag)") }
+    }
 }
 
 # $json.PSObject.Properties.Value
