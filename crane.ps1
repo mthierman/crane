94f0b2005f3c126eaaf8a8320889f932b7e92a1e
mthierman <mthierman@gmail.com>
2024-05-27 13:38:45 -0600

    Test

--- crane.ps1
+++ crane.ps1
@@ -4,45 +4,60 @@ if (Test-Path "libs") { Remove-Item "libs" -Force -Recurse }
 
 foreach ($package in $packages)
 {
-    $split = $package -split '[@/]'
+    $split = $package -split '[:/@]'
 
     $crane = [pscustomobject]@{
         user = $split[0]
         repo = $split[1]
         tag  = $split[2]
+        file = $split[3]
     }
 
     $crane | Format-List
 
     $tags = "https://github.com/$($crane.user)/$($crane.repo)/archive/refs/tags/$($crane.tag).zip"
     $heads = "https://github.com/$($crane.user)/$($crane.repo)/archive/refs/heads/$($crane.tag).zip"
+    $releases = "https://github.com/$($crane.user)/$($crane.repo)/releases/download/$($crane.tag)/$($crane.file).zip"
+
     $request = Invoke-WebRequest -Uri $tags -SkipHttpErrorCheck
 
-    if ($request.StatusCode -eq "200")
-    {
-        foreach ($response in $request)
-        {
-            $response.StatusCode
-            [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
-        }
-    }
-    else
-    {
-        $request = Invoke-WebRequest -Uri $heads -SkipHttpErrorCheck
+    if ($crane.file) { $crane.file }
+    else { $crane.repo }
 
-        if ($request.StatusCode -eq "200")
-        {
-            foreach ($response in $request)
-            {
-                $response.StatusCode
-                [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
-            }
-        }
-        else
-        {
-            gh release download -p "$($crane.tag)" -R "$($crane.user)/$($crane.repo)" --clobber
-            [System.IO.Compression.ZipFile]::ExtractToDirectory($($crane.tag), "libs/$($crane.user)/$($crane.repo)")
-            if (Test-Path "$($crane.tag)") { Remove-Item -Path "$($crane.tag)" -Force }
-        }
-    }
+    # if ($request.StatusCode -eq "200")
+    # {
+    #     foreach ($response in $request)
+    #     {
+    #         $response.StatusCode
+    #         [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
+    #     }
+    # }
+    # else
+    # {
+    #     $request = Invoke-WebRequest -Uri $heads -SkipHttpErrorCheck
+
+    #     if ($request.StatusCode -eq "200")
+    #     {
+    #         foreach ($response in $request)
+    #         {
+    #             $response.StatusCode
+    #             [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)")
+    #         }
+    #     }
+    #     else
+    #     {
+    #         $request = Invoke-WebRequest -Uri $releases -SkipHttpErrorCheck
+    #         if ($request.StatusCode -eq "200")
+    #         {
+    #             foreach ($response in $request)
+    #             {
+    #                 $response.StatusCode
+    #                 [System.IO.Compression.ZipFile]::ExtractToDirectory($response.RawContentStream, "libs/$($crane.user)/$($crane.repo)")
+    #             }
+    #         }
+    #         # gh release download -p "$($crane.tag)" -R "$($crane.user)/$($crane.repo)" --clobber
+    #         # [System.IO.Compression.ZipFile]::ExtractToDirectory($($crane.tag), "libs/$($crane.user)/$($crane.repo)")
+    #         # if (Test-Path "$($crane.tag)") { Remove-Item -Path "$($crane.tag)" -Force }
+    #     }
+    # }
 }
