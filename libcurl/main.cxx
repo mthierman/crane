2a43b30a1949a227a8a3db179fd293bbf4a9d132
mthierman <mthierman@gmail.com>
2024-05-27 20:59:00 -0600

    Cleanup C++ main

--- libcurl/main.cxx
+++ libcurl/main.cxx
@@ -1,15 +1,6 @@
 #include <print>
-#include <iostream>
 
 auto main() -> int
 {
-    std::println("{}", "Hello world");
-
-    return 0;
+    std::println("crane v0.0.0");
 }
-
-// auto main() -> int
-// {
-//     std::print("Test");
-//     std::println("crane v0.0.0");
-// }
