5e372bb929f51cf16c6ce503c8d5fd35d7e28e26
mthierman <mthierman@gmail.com>
2024-05-27 20:55:41 -0600

    Add C++ project

---
+++ libcurl/main.cxx
@@ -0,0 +1,15 @@
+#include <print>
+#include <iostream>
+
+auto main() -> int
+{
+    std::println("{}", "Hello world");
+
+    return 0;
+}
+
+// auto main() -> int
+// {
+//     std::print("Test");
+//     std::println("crane v0.0.0");
+// }
