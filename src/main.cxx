aadc268988f4778faab40270153e440ddf33ed1a
mthierman <mthierman@gmail.com>
2024-11-09 13:11:30 -0700

    Read file from AppData

--- src/main.cxx
+++ src/main.cxx
@@ -2,14 +2,33 @@
 
 #include <stdlib.h>
 
+#include <fstream>
+#include <iostream>
+
 #include <nlohmann/json.hpp>
 
-auto main() -> int {
-    if (auto path { glow::filesystem::known_folder(FOLDERID_Downloads) }) {
-        *path = *path / L"test.zip";
+using json = nlohmann::json;
 
-        glow::network::download_file(
-            "https://github.com/mthierman/crane/archive/refs/heads/main.zip", *path);
+auto main() -> int {
+    // if (auto path { glow::filesystem::known_folder(FOLDERID_Downloads) }) {
+    //     *path = *path / L"test.zip";
+
+    //     glow::network::download_file(
+    //         "https://github.com/mthierman/crane/archive/refs/heads/main.zip", *path);
+    // }
+
+    //
+
+    if (auto path { glow::filesystem::known_folder() }) {
+        *path = *path / L"crane" / L"crane.json";
+
+        auto stream { std::ifstream(*path) };
+
+        if (stream.is_open()) {
+            std::cout << "Reading crane.json..." << std::endl;
+            auto data = json::parse(stream);
+            std::cout << std::setw(4) << data << std::endl;
+        }
     }
 
     return EXIT_SUCCESS;
