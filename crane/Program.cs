b952653576eb7e8e49256c985ca768820e93fa2a
mthierman <mthierman@gmail.com>
2024-11-08 22:24:44 -0700

    Remove C# project and update CMake

--- crane/Program.cs
+++
@@ -1,7 +0,0 @@
-// https://github.com/dotnet/command-line-api/issues/2338
-// https://learn.microsoft.com/en-us/dotnet/standard/commandline/
-// https://github.com/dotnet/command-line-api
-// https://www.nuget.org/packages/System.CommandLine
-
-Console.WriteLine("crane v0.0.0");
-Console.ReadKey();
