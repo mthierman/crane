1bb9b2ade6d91477b4f53f4447d33201e7767030
mthierman <mthierman@gmail.com>
2024-11-08 23:41:51 -0700

    Use Glow

---
+++ cmake/Glow.cmake
@@ -0,0 +1,8 @@
+FetchContent_Declare(
+    Glow
+    GIT_REPOSITORY "https://github.com/mthierman/Glow.git"
+    GIT_TAG next
+    GIT_SHALLOW ON
+    )
+
+FetchContent_MakeAvailable(Glow)
--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,14 @@
-#include <print>
+#include <glow/glow.hxx>
 
-auto main() -> int
-{
-    std::println("crane v0.0.0");
+#include <stdlib.h>
+
+auto main() -> int {
+    if (auto path { glow::filesystem::known_folder(FOLDERID_Downloads) }) {
+        *path = *path / L"test.zip";
+
+        glow::network::download_file(
+            "https://github.com/mthierman/crane/archive/refs/heads/main.zip", *path);
+    }
+
+    return EXIT_SUCCESS;
 }
