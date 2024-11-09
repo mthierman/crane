f07b6c3ee6eee21af3f7f8f124cabaa93b394738
mthierman <mthierman@gmail.com>
2024-11-09 13:28:06 -0700

    Read json file and iterate vector

--- src/main.cxx
+++ src/main.cxx
@@ -4,10 +4,18 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
+
+struct Manifest {
+    std::vector<std::string> packages;
+
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Manifest, packages);
+};
 
 auto main() -> int {
     // if (auto path { glow::filesystem::known_folder(FOLDERID_Downloads) }) {
@@ -27,7 +35,21 @@ auto main() -> int {
         if (stream.is_open()) {
             std::cout << "Reading crane.json..." << std::endl;
             auto data = json::parse(stream);
-            std::cout << std::setw(4) << data << std::endl;
+            Manifest manifest;
+            manifest.packages = data["packages"].get<std::vector<std::string>>();
+
+            for (auto& package : manifest.packages) {
+                std::cout << package << std::endl;
+            }
+
+            // json j = manifest;
+            // auto manifest { j.get<Manifest>() };
+
+            // manifest = data.get<Manifest>();
+
+            // auto packages { data.get<std::vector<std::string>>() };
+            // for (auto& package&)
+            // std::cout << std::setw(4) << data << std::endl;
         }
     }
 
