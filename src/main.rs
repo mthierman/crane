022e77fcd5e251ad82051fe1f9aa4c1643e7a07f
mthierman <mthierman@gmail.com>
2024-11-10 15:43:49 -0700

    Better install log

--- src/main.rs
+++ src/main.rs
@@ -52,30 +52,30 @@ fn main() {
 
                 match split[0] {
                     "gh" => {
-                        // let repo = split[1].split("@").next().unwrap();
-                        // let branch = split[1].split("@").nth(1).unwrap();
-                        // println!("{} - {}", repo, branch);
+                        let repo = split[1].split("@").next().unwrap();
+                        let branch = split[1].split("@").nth(1).unwrap();
+                        println!("Installing {}@{}", repo, branch);
 
                         let output = Command::new("gh")
                             .current_dir(&package_cache)
                             .args(["repo", "clone", repo, "--", "--branch", branch, "--depth=1"])
                             .output()
                             .unwrap();
-                        let printout = String::from_utf8(output.stdout).unwrap();
-                        println!("{}", printout);
+                        // let printout = String::from_utf8(output.stdout).unwrap();
+                        // println!("{}", printout);
                     }
                     "nuget" => {
-                        // let package = split[1].split("@").next().unwrap();
-                        // let version = split[1].split("@").nth(1).unwrap();
-                        // println!("{} - {}", package, version);
+                        let package = split[1].split("@").next().unwrap();
+                        let version = split[1].split("@").nth(1).unwrap();
+                        println!("Installing {}@{}...", package, version);
 
                         let output = Command::new("nuget")
                             .current_dir(&package_cache)
                             .args(["install", package, "-Version", version])
                             .output()
                             .unwrap();
-                        let printout = String::from_utf8(output.stdout).unwrap();
-                        println!("{}", printout);
+                        // let printout = String::from_utf8(output.stdout).unwrap();
+                        // println!("{}", printout);
                     }
                     _ => {
                         println!("ERROR!")
