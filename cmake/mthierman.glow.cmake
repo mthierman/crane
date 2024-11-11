62122391739f2712c35f18e3e81ce01d2a9e7e2e
mthierman <mthierman@gmail.com>
2024-11-10 20:42:46 -0700

    Remove C++ implementation

--- cmake/mthierman.glow.cmake
+++
@@ -1,8 +0,0 @@
-FetchContent_Declare(
-    mthierman_glow
-    GIT_REPOSITORY "https://github.com/mthierman/Glow.git"
-    GIT_TAG next
-    GIT_SHALLOW ON
-    )
-
-FetchContent_MakeAvailable(mthierman_glow)
