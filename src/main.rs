eeb7db6000cfab876b0419b11dec672595c642cd
mthierman <mthierman@gmail.com>
2024-11-21 19:11:08 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use crane::*;
 use windows::Win32::UI::Shell::*;
 
 fn main() {
-    println!("{}", common::known_folder(&FOLDERID_LocalAppData).display());
-
     let crane = Crane::new();
 
     crane.run();
